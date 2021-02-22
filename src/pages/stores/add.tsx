f9a8e0f7526c3677ae1e13d2a3ada0c95862556f
Ali <Mi3LiX9@Outlook.sa>
2021-02-23 01:38:36 +0300

    add AddStore Mutation in the page

--- src/pages/stores/add.tsx
+++ src/pages/stores/add.tsx
@@ -1,7 +1,34 @@
+import { gql, useMutation } from "@apollo/client";
 import { NextPage } from "next";
 
 const AddStorePage: NextPage = () => {
-  return <div></div>;
+  const [mutate, { data, loading, error }] = useMutation(ADD_STORE_MUTAION);
+
+  return (
+    <div>
+      <button
+        onClick={() =>
+          mutate({
+            variables: { title: "test", description: "test", icon: "test" },
+          })
+        }
+      >
+        اضغطني عشان اضف تست
+      </button>
+      <pre>{JSON.stringify({ data, loading, error }, null, 2)}</pre>
+    </div>
+  );
 };
+
+const ADD_STORE_MUTAION = gql`
+  mutation AddStore($title: String!, $description: String, $icon: String!) {
+    createStore(
+      data: { title: $title, description: $description, icon: $icon }
+    ) {
+      title
+      icon
+    }
+  }
+`;
 
 export default AddStorePage;
