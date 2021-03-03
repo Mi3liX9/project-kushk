781a0a7245139bf47d5ce302071736f917b95c19
Ali <Mi3LiX9@Outlook.sa>
2021-03-04 01:29:57 +0300

    add AuthHook

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,28 @@
+import { NextPage } from "next";
+import { createContext } from "react";
+import { useAuth } from "src/hooks/authHook";
+import { auth, googleAuthProvider } from "src/utils/firebase/firebase";
+
+const AuthPage: NextPage = () => {
+  const { user } = useAuth();
+
+  const signInWithGoogle = async () => {
+    auth.signInWithPopup(googleAuthProvider);
+  };
+
+  const signOut = () => {
+    auth.signOut();
+  };
+  return (
+    <div>
+      <h2>{user?.email}</h2>
+      {!user ? (
+        <button onClick={signInWithGoogle}>sign in</button>
+      ) : (
+        <button onClick={signOut}>sign out</button>
+      )}
+    </div>
+  );
+};
+
+export default AuthPage;
--- src/utils/firebase/firebase.ts
+++ src/utils/firebase/firebase.ts
@@ -2,18 +2,20 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBAE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBAE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-firebase.auth().useDeviceLanguage();
+// firebase.auth().useDeviceLanguage();
+
 export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
